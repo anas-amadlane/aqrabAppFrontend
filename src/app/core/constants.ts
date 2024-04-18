53246e60a78eb45e6f512cac0d39aa13df6328e9
anas amadlane <anasamadlane@gmail.com>
2024-04-18 20:38:14 +0100

    New Changes

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -1,6 +1,6 @@
 
 export const Conf= Object.freeze({
-  envUrl:'https://enshaalah-cj4uxmvvwq-uc.a.run.app/aqrab/api' ,// 'Prod Url',
+  envUrl:'http://localhost:8080/aqrab/api' ,// 'Prod Url',
   appContext: '/aqrab',
   apiUrl:'/api',
 })
---
+++ src/app_old/core/constants.ts
@@ -0,0 +1,6 @@
+
+export const Conf= Object.freeze({
+  envUrl:'https://enshaalah-cj4uxmvvwq-uc.a.run.app/aqrab/api' ,// 'Prod Url',
+  appContext: '/aqrab',
+  apiUrl:'/api',
+})
